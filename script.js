4fb5da0905cea9b7c7c629bbd4ea6b1eaa88aa35
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-05-03 16:33:24 +0100

    Cards able to be clicked, and will compare

--- script.js
+++ script.js
@@ -16,15 +16,53 @@ const cardContainer = document.querySelector(".card-container");
 
 //////////Functions/////////////
 let cardsShowing = 0;
+let animals = ["cat", "monkey", "cat", "penguin", "penguin", "monkey"];
+let showingArr = [];
 
 // Function that reveals a picture associated with the card
-const revealCard = function () {
-  // For card clicked display the picture associated.
-  // If clicked reveal img
+const revealCard = function (e) {
+  const dataValue = e.target.dataset.cards;
+  const displayCard = e.target.closest(".card");
+
+  displayCard.innerHTML = `
+    <img class="" src="img/${animals[dataValue]}.png" alt="Monkey">
+    `;
+
+  displayCard.style.backgroundColor = "#454545";
+  showingArr.push(dataValue);
+  cardsShowing++;
+  console.log(cardsShowing);
+  console.log(showingArr);
 };
 
-const checkPair = function () {
-  // If two cards are displayed check if the cards match
+const checkPair = function (e) {
+  if (cardsShowing === 2) {
+    if (showingArr.includes("1") && showingArr.includes("5")) {
+      console.log("winner");
+      showingArr = [];
+    } else if (showingArr.includes("0") && showingArr.includes("2")) {
+      console.log("winner");
+      showingArr = [];
+    } else if (showingArr.includes("3") && showingArr.includes("4")) {
+      console.log("winner");
+      showingArr = [];
+    } else {
+      setTimeout(function () {
+        cardsShowing = 0;
+
+        showingArr = [];
+
+        allCards.forEach(function (c) {
+          c.innerHTML = "";
+          c.style.backgroundColor = "#ff6000";
+        });
+      }, 1000);
+    }
+  }
+
+  if (cardsShowing === 6) {
+    console.log("Congratulations, you win!");
+  }
 };
 
 // function that checks what a card is so that the right picture can be diplayed
@@ -32,55 +70,57 @@ const checkData = function () {};
 
 //////////Events////////////
 cardContainer.addEventListener("click", function (e) {
-  if (e.target.dataset.cards === "1") {
-    const displayCard = e.target.closest(".card");
-    displayCard.innerHTML = `
-      <img class="" src="img/monkey.png" alt="Monkey">
-      `;
-    displayCard.style.backgroundColor = "#454545";
-  }
+  revealCard(e);
+  checkPair();
+  //   if (e.target.dataset.cards === "1") {
+  //     const displayCard = e.target.closest(".card");
+  //     displayCard.innerHTML = `
+  //       <img class="" src="img/monkey.png" alt="Monkey">
+  //       `;
+  //     displayCard.style.backgroundColor = "#454545";
+  //   }
 
-  if (e.target.dataset.cards === "5") {
-    const displayCard = e.target.closest(".card");
-    displayCard.innerHTML = `
-        <img class="" src="img/monkey.png" alt="Monkey">
-        `;
-    displayCard.style.backgroundColor = "#454545";
-  }
+  //   if (e.target.dataset.cards === "5") {
+  //     const displayCard = e.target.closest(".card");
+  //     displayCard.innerHTML = `
+  //         <img class="" src="img/monkey.png" alt="Monkey">
+  //         `;
+  //     displayCard.style.backgroundColor = "#454545";
+  //   }
 
-  if (e.target.dataset.cards === "0") {
-    const displayCard = e.target.closest(".card");
-    displayCard.innerHTML = `
-        <img class="" src="img/cat.png" alt="cat">
-        `;
-    displayCard.style.backgroundColor = "#454545";
-  }
+  //   if (e.target.dataset.cards === "0") {
+  //     const displayCard = e.target.closest(".card");
+  //     displayCard.innerHTML = `
+  //         <img class="" src="img/cat.png" alt="cat">
+  //         `;
+  //     displayCard.style.backgroundColor = "#454545";
+  //   }
 
-  if (e.target.dataset.cards === "2") {
-    const displayCard = e.target.closest(".card");
-    displayCard.innerHTML = `
-        <img class="" src="img/cat.png" alt="cat">
-        `;
-    displayCard.style.backgroundColor = "#454545";
-  }
+  //   if (e.target.dataset.cards === "2") {
+  //     const displayCard = e.target.closest(".card");
+  //     displayCard.innerHTML = `
+  //         <img class="" src="img/cat.png" alt="cat">
+  //         `;
+  //     displayCard.style.backgroundColor = "#454545";
+  //   }
 
-  if (e.target.dataset.cards === "3") {
-    const displayCard = e.target.closest(".card");
-    displayCard.innerHTML = `
-        <img class="" src="img/penguin.png" alt="penguin">
-        `;
-    displayCard.style.backgroundColor = "#454545";
-  }
+  //   if (e.target.dataset.cards === "3") {
+  //     const displayCard = e.target.closest(".card");
+  //     displayCard.innerHTML = `
+  //         <img class="" src="img/penguin.png" alt="penguin">
+  //         `;
+  //     displayCard.style.backgroundColor = "#454545";
+  //   }
 
-  if (e.target.dataset.cards === "4") {
-    const displayCard = e.target.closest(".card");
-    displayCard.innerHTML = `
-        <img class="" src="img/penguin.png" alt="penguin">
-        `;
-    displayCard.style.backgroundColor = "#454545";
-  }
-  console.log(e.target);
-  console.log(e.currentTarget);
+  //   if (e.target.dataset.cards === "4") {
+  //     const displayCard = e.target.closest(".card");
+  //     displayCard.innerHTML = `
+  //         <img class="" src="img/penguin.png" alt="penguin">
+  //         `;
+  //     displayCard.style.backgroundColor = "#454545";
+  //   }
+  //   console.log(e.target);
+  //   console.log(e.currentTarget);
 });
 
 // allCards.forEach((card, i) =>
