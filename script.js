5d303ee2df2d09c0e6ed74815ec5697357bff28f
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-05-04 10:20:49 +0100

    Logic working, okay, needs to clear false cards

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 "use strict";
 // Game components
 /* 
-1. When a card is clicked it reveals a picture
-2. When 2 cards have been chosen they are compared and if they match then they are removed and the user recieve a point.
-3. If the cards do not match the cards flip back over.
-4.The slider button goes left and right and changes the colour sceam of the baord but not the buttons atm 
-5.
+TODO
+- Make sure each card can only be clicked once
+- Create slider function
+
 
 */
 
@@ -16,6 +15,7 @@ const cardContainer = document.querySelector(".card-container");
 
 //////////Functions/////////////
 let cardsShowing = 0;
+let correctPairs = 0;
 let animals = ["cat", "monkey", "cat", "penguin", "penguin", "monkey"];
 let showingArr = [];
 
@@ -36,26 +36,46 @@ const revealCard = function (e) {
 };
 
 const checkPair = function (e) {
+  let whichCar = e.target.closest.card.dataset.cards;
   if (cardsShowing === 2) {
     if (showingArr.includes("1") && showingArr.includes("5")) {
       console.log("winner");
       showingArr = [];
+      correctPairs++;
+      cardsShowing = 0;
+      document.querySelector("[data-cards='1']").style.backgroundColor =
+        "limegreen";
+      document.querySelector("[data-cards='5']").style.backgroundColor =
+        "limegreen";
     } else if (showingArr.includes("0") && showingArr.includes("2")) {
       console.log("winner");
       showingArr = [];
+      correctPairs++;
+      cardsShowing = 0;
+      document.querySelector("[data-cards='0']").style.backgroundColor =
+        "limegreen";
+      document.querySelector("[data-cards='2']").style.backgroundColor =
+        "limegreen";
     } else if (showingArr.includes("3") && showingArr.includes("4")) {
       console.log("winner");
       showingArr = [];
+      correctPairs++;
+      cardsShowing = 0;
+      document.querySelector("[data-cards='3']").style.backgroundColor =
+        "limegreen";
+      document.querySelector("[data-cards='4']").style.backgroundColor =
+        "limegreen";
     } else {
       setTimeout(function () {
         cardsShowing = 0;
-
-        showingArr = [];
 
         allCards.forEach(function (c) {
           c.innerHTML = "";
           c.style.backgroundColor = "#ff6000";
         });
+
+        showingArr = [];
+        // For each card that data number does not equal any of the numbers in correctArr reset to default orange
       }, 1000);
     }
   }
@@ -65,67 +85,8 @@ const checkPair = function (e) {
   }
 };
 
-// function that checks what a card is so that the right picture can be diplayed
-const checkData = function () {};
-
 //////////Events////////////
 cardContainer.addEventListener("click", function (e) {
   revealCard(e);
   checkPair();
-  //   if (e.target.dataset.cards === "1") {
-  //     const displayCard = e.target.closest(".card");
-  //     displayCard.innerHTML = `
-  //       <img class="" src="img/monkey.png" alt="Monkey">
-  //       `;
-  //     displayCard.style.backgroundColor = "#454545";
-  //   }
-
-  //   if (e.target.dataset.cards === "5") {
-  //     const displayCard = e.target.closest(".card");
-  //     displayCard.innerHTML = `
-  //         <img class="" src="img/monkey.png" alt="Monkey">
-  //         `;
-  //     displayCard.style.backgroundColor = "#454545";
-  //   }
-
-  //   if (e.target.dataset.cards === "0") {
-  //     const displayCard = e.target.closest(".card");
-  //     displayCard.innerHTML = `
-  //         <img class="" src="img/cat.png" alt="cat">
-  //         `;
-  //     displayCard.style.backgroundColor = "#454545";
-  //   }
-
-  //   if (e.target.dataset.cards === "2") {
-  //     const displayCard = e.target.closest(".card");
-  //     displayCard.innerHTML = `
-  //         <img class="" src="img/cat.png" alt="cat">
-  //         `;
-  //     displayCard.style.backgroundColor = "#454545";
-  //   }
-
-  //   if (e.target.dataset.cards === "3") {
-  //     const displayCard = e.target.closest(".card");
-  //     displayCard.innerHTML = `
-  //         <img class="" src="img/penguin.png" alt="penguin">
-  //         `;
-  //     displayCard.style.backgroundColor = "#454545";
-  //   }
-
-  //   if (e.target.dataset.cards === "4") {
-  //     const displayCard = e.target.closest(".card");
-  //     displayCard.innerHTML = `
-  //         <img class="" src="img/penguin.png" alt="penguin">
-  //         `;
-  //     displayCard.style.backgroundColor = "#454545";
-  //   }
-  //   console.log(e.target);
-  //   console.log(e.currentTarget);
 });
-
-// allCards.forEach((card, i) =>
-//   card.addEventListener("click", function (e) {
-
-//     // Img is associated with 2 data values
-//   })
-// );
